a4e512bc5e2925d4332f28fc998a50c236bcb487
Chris Woosley <woosleyc@vystarcu.org>
2021-03-17 09:54:55 -0400

    update

---
+++ src/Controllers/CampsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CampsController : ControllerBase
+    {
+        public CampsController()
+        {
+
+        }
+
+        //public object Get()
+        //{
+        //    return new { Moniker = "TJones", Name = "Tom Jones" };
+        //}
+
+        [HttpGet(Name = "GetCamps")]
+        public IActionResult GetCamps()
+        {
+            return Ok(new { Moniker = "TJones", Name = "Tom Jones" });
+        }
+    }
+}
